8a38dcab76c84643df64cd7bd663e200e122eb79
joao dias <joaoeduardodias123@gmail.com>
2021-03-28 15:24:19 -0300

    Create repositories of categories

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,17 +1,17 @@
 import { Router } from "express";
 
 import { CategoriesRepository } from "../repositories/CategoriesRepository";
+import { CreateCategoryService } from "../services/CreateCategoryService";
 
 const categoriesRepository = new CategoriesRepository();
 const categoriesRoutes = Router();
 
 categoriesRoutes.post("/", (request, response) => {
    const { name, description } = request.body;
-   const categoryAlreadyExists = categoriesRepository.findByName(name);
-   if (categoryAlreadyExists) {
-      return response.status(400).json({ error: "Category already exist" });
-   }
-   categoriesRepository.create({ name, description });
+   const createCategoryService = new CreateCategoryService(
+      categoriesRepository
+   );
+   createCategoryService.execute({ name, description });
    return response.status(201).send();
 });
 categoriesRoutes.get("/", (request, response) => {
