8d5697ce00fc21b20ac2c0c3faa2b205ee1067ad
joao dias <joaoeduardodias123@gmail.com>
2021-03-28 13:22:10 -0300

    list categories

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,21 +1,18 @@
-import { Router } from "express";
+import { request, response, Router } from "express";
 
-import { Category } from "../model/Category";
+import { CategoriesRepository } from "../repositories/CategoriesRepository";
 
+const categoriesRepository = new CategoriesRepository();
 const categoriesRoutes = Router();
-const categories: Category[] = [];
 
 categoriesRoutes.post("/", (request, response) => {
    const { name, description } = request.body;
-   const category = new Category();
-   Object.assign(category, {
-      name,
-      description,
-      created_at: new Date(),
-   });
-
-   categories.push(category);
-   return response.status(201).json(category);
+   categoriesRepository.create({ name, description });
+   return response.status(201).send();
+});
+categoriesRoutes.get("/", (request, response) => {
+   const all = categoriesRepository.list();
+   return response.json(all);
 });
 
 export { categoriesRoutes };
