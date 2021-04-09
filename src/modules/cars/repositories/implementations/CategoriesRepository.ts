da7afd08463116fa28135c014407fbdc1b74363a
Joao Dias <joaoeduardodias123@gmail.com>
2021-04-09 09:41:14 -0300

    Create categories in BD postgresql

--- src/modules/cars/repositories/implementations/CategoriesRepository.ts
+++ src/modules/cars/repositories/implementations/CategoriesRepository.ts
@@ -1,38 +1,29 @@
-import { Category } from "../../model/Category";
+import { getRepository, Repository } from "typeorm";
+
+import { Category } from "../../entities/Category";
 import {
    ICategoriesRepository,
    ICreateCategoryDTO,
 } from "../ICategoriesRepository";
 
 class CategoriesRepository implements ICategoriesRepository {
-   private categories: Category[];
-   private static INSTANCE: CategoriesRepository;
-   private constructor() {
-      this.categories = [];
-   }
-   public static getInstance(): CategoriesRepository {
-      if (!CategoriesRepository.INSTANCE) {
-         CategoriesRepository.INSTANCE = new CategoriesRepository();
-      }
-      return CategoriesRepository.INSTANCE;
-   }
-   create({ name, description }: ICreateCategoryDTO): void {
-      const category = new Category();
-      Object.assign(category, {
-         name,
-         description,
-         created_at: new Date(),
-      });
+   private repository: Repository<Category>;
 
-      this.categories.push(category);
+   constructor() {
+      this.repository = getRepository(Category);
    }
-   list(): Category[] {
-      return this.categories;
+
+   async create({ name, description }: ICreateCategoryDTO): Promise<void> {
+      const category = this.repository.create({ name, description });
+
+      await this.repository.save(category);
    }
-   findByName(name: string): Category {
-      const category = this.categories.find(
-         (category) => category.name === name
-      );
+   async list(): Promise<Category[]> {
+      const categories = await this.repository.find();
+      return categories;
+   }
+   async findByName(name: string): Promise<Category> {
+      const category = await this.repository.findOne({ name });
       return category;
    }
 }
--- src/modules/cars/useCases/createCategory/CreateCategoryUseCase.ts
+++ src/modules/cars/useCases/createCategory/CreateCategoryUseCase.ts
@@ -7,8 +7,10 @@ interface IRequest {
 
 class CreateCategoryUseCase {
    constructor(private categoriesRepository: ICategoriesRepository) {}
-   execute({ description, name }: IRequest): void {
-      const categoryAlreadyExists = this.categoriesRepository.findByName(name);
+   async execute({ description, name }: IRequest): Promise<void> {
+      const categoryAlreadyExists = await this.categoriesRepository.findByName(
+         name
+      );
       if (categoryAlreadyExists) {
          throw new Error("Category already exist!");
       }
