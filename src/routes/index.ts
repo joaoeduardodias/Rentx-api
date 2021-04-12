bcbf8898725f47127370af971c46e1183f26788c
Joao Dias <joaoeduardodias123@gmail.com>
2021-04-12 14:07:52 -0300

    Incrementation documentation

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -6,6 +6,6 @@ import { specificationRoutes } from "./specification.routes";
 const router = Router();
 
 router.use("/categories", categoriesRoutes);
-router.use("/spacifications", specificationRoutes);
+router.use("/specifications", specificationRoutes);
 
 export { router };
