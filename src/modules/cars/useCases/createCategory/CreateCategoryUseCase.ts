b530989ce5c6f84ed68fb72287cd74aea443d450
Joao Dias <joaoeduardodias123@gmail.com>
2021-04-12 11:17:29 -0300

    Refactoring injection dependences

--- src/modules/cars/useCases/createCategory/CreateCategoryUseCase.ts
+++ src/modules/cars/useCases/createCategory/CreateCategoryUseCase.ts
@@ -1,12 +1,17 @@
+import { inject, injectable } from "tsyringe";
+
 import { ICategoriesRepository } from "../../repositories/ICategoriesRepository";
 
 interface IRequest {
    name: string;
    description: string;
 }
-
+@injectable()
 class CreateCategoryUseCase {
-   constructor(private categoriesRepository: ICategoriesRepository) {}
+   constructor(
+      @inject("CategoriesRepository")
+      private categoriesRepository: ICategoriesRepository
+   ) {}
    async execute({ description, name }: IRequest): Promise<void> {
       const categoryAlreadyExists = await this.categoriesRepository.findByName(
          name
@@ -14,7 +19,7 @@ class CreateCategoryUseCase {
       if (categoryAlreadyExists) {
          throw new Error("Category already exist!");
       }
-      this.categoriesRepository.create({ name, description });
+      await this.categoriesRepository.create({ name, description });
    }
 }
 
