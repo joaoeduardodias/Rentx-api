d63cc9fb2c5ce9a34c39518c679248ccb7fe3559
Joao Dias <joaoeduardodias123@gmail.com>
2021-04-23 15:18:07 -0300

    Create seed of create user admin

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -1,13 +1,15 @@
+import "reflect-metadata";
 import express, { NextFunction, Request, Response } from "express";
 import "express-async-errors";
 import swaggerUi from "swagger-ui-express";
 
-import "../typeorm";
 import "../../container";
 import swaggerFile from "../../../swagger.json";
 import { AppError } from "../../errors/AppError";
+import createConnection from "../typeorm";
 import { router } from "./routes";
 
+createConnection();
 const app = express();
 app.use(express.json());
 
