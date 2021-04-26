03444ee510a31336711811f3e52f5900e86668df
Joao Dias <joaoeduardodias123@gmail.com>
2021-04-26 15:36:47 -0300

    list cars

---
+++ src/modules/cars/useCases/listAvailableCars/ListAvailableCarUseCase.spec.ts
@@ -0,0 +1,76 @@
+import { CarsRepositoryInMemory } from "../../repositories/in-menory/CarsRepositoryInMemory";
+import { ListAvailableCarsUseCase } from "./ListAvailableCarsUseCase";
+
+let listAvailableCarsUseCase: ListAvailableCarsUseCase;
+let carsRepositoryInMemory: CarsRepositoryInMemory;
+describe("List Cars", () => {
+   beforeEach(() => {
+      carsRepositoryInMemory = new CarsRepositoryInMemory();
+      listAvailableCarsUseCase = new ListAvailableCarsUseCase(
+         carsRepositoryInMemory
+      );
+   });
+   it("should be able to list all available cars", async () => {
+      const car = await carsRepositoryInMemory.create({
+         name: "Car1",
+         description: "Car description",
+         daily_rate: 40.0,
+         license_plate: "ABC-1212",
+         fine_amount: 100,
+         brand: "Car_brand",
+         category_id: "category_id",
+      });
+
+      const cars = await listAvailableCarsUseCase.execute({});
+
+      expect(cars).toEqual([car]);
+   });
+   it("should be able to list available cars by brand", async () => {
+      const car = await carsRepositoryInMemory.create({
+         name: "Car1",
+         description: "Car description",
+         daily_rate: 40.0,
+         license_plate: "ABC-1212",
+         fine_amount: 100,
+         brand: "Car_brand_test",
+         category_id: "category_id",
+      });
+
+      const cars = await listAvailableCarsUseCase.execute({
+         brand: "Car_brand_test",
+      });
+      expect(cars).toEqual([car]);
+   });
+   it("should be able to list available cars by name", async () => {
+      const car = await carsRepositoryInMemory.create({
+         name: "Car_3",
+         description: "Car description",
+         daily_rate: 40.0,
+         license_plate: "ABC-12345",
+         fine_amount: 100,
+         brand: "Car_brand_test",
+         category_id: "category_id",
+      });
+
+      const cars = await listAvailableCarsUseCase.execute({
+         name: "Car_3",
+      });
+      expect(cars).toEqual([car]);
+   });
+   it("should be able to list available cars by category", async () => {
+      const car = await carsRepositoryInMemory.create({
+         name: "Car_3",
+         description: "Car description",
+         daily_rate: 40.0,
+         license_plate: "ABC-12345",
+         fine_amount: 100,
+         brand: "Car_brand_test",
+         category_id: "12345",
+      });
+
+      const cars = await listAvailableCarsUseCase.execute({
+         category_id: "12345",
+      });
+      expect(cars).toEqual([car]);
+   });
+});
--- src/shared/infra/http/routes/cars.routes.ts
+++ src/shared/infra/http/routes/cars.routes.ts
@@ -1,12 +1,14 @@
 import { Router } from "express";
 
 import { CreateCarController } from "../../../../modules/cars/useCases/createCar/CreateCarController";
+import { ListAvailableCarsController } from "../../../../modules/cars/useCases/listAvailableCars/ListAvailableCarsController";
 import { ensureAdmin } from "../middlewares/ensureAdmin";
 import { ensureAuthenticated } from "../middlewares/ensureAuthenticated";
 
 const carsRoutes = Router();
 
 const createCarcontroller = new CreateCarController();
+const listAvailableCarsController = new ListAvailableCarsController();
 
 carsRoutes.post(
    "/",
@@ -14,5 +16,6 @@ carsRoutes.post(
    ensureAdmin,
    createCarcontroller.handle
 );
+carsRoutes.get("/available", listAvailableCarsController.handle);
 
 export { carsRoutes };
