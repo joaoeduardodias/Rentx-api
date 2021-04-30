9edc215c34b5afcb167814fa47983d50f7d589e8
Joao Dias <joaoeduardodias123@gmail.com>
2021-04-30 13:55:44 -0300

    create table rentals

--- src/shared/infra/typeorm/migrations/1618249938174-CreateUsers.ts
+++ src/shared/infra/typeorm/migrations/1618249938174-CreateUsers.ts
@@ -9,6 +9,7 @@ export class CreateUsers1618249938174 implements MigrationInterface {
                {
                   name: "id",
                   type: "uuid",
+                  isPrimary: true,
                },
                {
                   name: "name",
