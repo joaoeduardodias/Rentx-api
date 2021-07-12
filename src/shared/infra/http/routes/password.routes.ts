a1e696fa942025d96a54426d073f87de2c25cc5b
Joao Dias <joaoeduardodias123@gmail.com>
2021-07-12 15:09:30 -0300

    Create reset password

--- src/shared/infra/http/routes/password.routes.ts
+++ src/shared/infra/http/routes/password.routes.ts
@@ -1,10 +1,13 @@
 import { Router } from "express";
 
+import { ResetPasswordUserController } from "../../../../modules/accounts/useCases/resetPasswordUser/ResetPasswordUserController";
 import { SendForgotPasswordMaiController } from "../../../../modules/accounts/useCases/sendForgotPasswordMail/SendForgotPasswordMaiController";
 
 const passwordRoutes = Router();
 const sendForgotPasswordMailController = new SendForgotPasswordMaiController();
+const resetPasswordController = new ResetPasswordUserController();
 
 passwordRoutes.post("/forgot", sendForgotPasswordMailController.handle);
+passwordRoutes.post("/reset", resetPasswordController.handle);
 
 export { passwordRoutes };
