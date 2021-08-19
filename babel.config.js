ed9232a34602ed7feaeff3434ab67533210692a7
Joao Dias <joaoeduardodias123@gmail.com>
2021-08-19 11:04:30 -0300

    remove libraries

--- babel.config.js
+++ babel.config.js
@@ -1,15 +1,12 @@
-module.exports = {
+
+ module.exports = {
    presets: [
-     ["@babel/preset-env", { targets: { node: "current" } }],
-     "@babel/preset-typescript",
+      ["@babel/preset-env", { targets: { node: "current" }, loose: true }],
+      "@babel/preset-typescript",
    ],
    plugins: [
-     ["module-resolver"],
-     "babel-plugin-transform-typescript-metadata",
-     ["@babel/plugin-proposal-decorators", { legacy: true }],
-     ["@babel/plugin-proposal-class-properties", { loose: true }],
-     ["@babel/plugin-proposal-private-property-in-object", { "loose": true }],
-
+      "babel-plugin-transform-typescript-metadata",
+      ["@babel/plugin-proposal-decorators", { legacy: true }],
+      ["@babel/plugin-proposal-class-properties", { loose: true }],
    ],
-
- };
+};
